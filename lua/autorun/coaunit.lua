03d49d867d85b8df9629fe8e6f9961e155e2b880
nightlifex <steven222giese@gmail.com>
2018-08-31 23:07:15 +0200

    Initial commit

---
+++ lua/autorun/coaunit.lua
@@ -0,0 +1,2 @@
+player_manager.AddValidModel( "COAUnit", "models/prospekt nova/pm_protoguard/pm_protoguard.mdl" );
+list.Set( "PlayerOptionsModel", "COAUnit", "models/prospekt nova/pm_protoguard/pm_protoguard.mdl" );
